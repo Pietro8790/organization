498764206ac39b084466f1f1245f937f86a7fa7f
Pietro Filipi <pfedo44@gmail.com>
2023-01-03 14:36:22 +0100

    deleted brakets on the name of region, name of s3 bucket and dynamodb table

--- state/backend.tf
+++ state/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "{{pierre-terraform-s3-bucket}}"
+    bucket         = "pierre-terraform-s3-bucket"
     key            = "terraform.tfstate"
-    region         = "{{us-east-1}}"
-    dynamodb_table = "{{dynamodb-state-locking}}"
+    region         = "us-east-1"
+    dynamodb_table = "dynamodb-state-locking"
   }
 }
