963a48e5ffac83b034876665cce2147239160704
Pietro Filipi <pfedo44@gmail.com>
2023-01-03 14:33:14 +0100

    change the aws region name back to us-east-1

--- state/backend.tf
+++ state/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "{{pierre-terraform-s3-bucket}}"
     key            = "terraform.tfstate"
-    region         = "{{US East (N. Virginia) us-east-1}}"
+    region         = "{{us-east-1}}"
     dynamodb_table = "{{dynamodb-state-locking}}"
   }
 }
